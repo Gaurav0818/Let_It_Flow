c7119836631714c478235a36f2898510bbb79132
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-05-11 01:10:39 +0530

    size dec

---
+++ Assets/Script/SizeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeController : MonoBehaviour
+{
+    float time=3;
+    private void Update()
+    {
+        time = time - Time.deltaTime;
+        Debug.Log(time);
+        if (time < 0)
+        {
+            gameObject.transform.localScale = gameObject.transform.localScale - new Vector3(0.1f, 0.1f, 0);
+            time = 3;
+        }
+    }
+}
