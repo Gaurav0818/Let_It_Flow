64bb10609ccf865d2a6a0c23266856bede157965
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-05-10 19:51:43 +0530

    drag added

---
+++ Assets/Script/FrictionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "N_Ground")
+        {
+            rb.angularDrag = 1;
+        }
+        if (collision.gameObject.tag == "HF_Ground")
+        {
+            rb.angularDrag = 3;
+        }
+    }
+}
