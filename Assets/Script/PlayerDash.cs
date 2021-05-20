2224304673c2ac614792e9d80b14188dc54286a1
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-05-20 12:29:30 +0530

    Dash Added

---
+++ Assets/Script/PlayerDash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public float dashSpeed;
+    public float dashTime;
+    public float startDastTime;
+    private int direction;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        dashTime = startDastTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                dashTime = startDastTime;
+                rb.velocity = Vector2.left * dashTime;
+            }
+        }
+    }
+}
--- Assets/Script/VelocityController.cs
+++ Assets/Script/VelocityController.cs
@@ -6,6 +6,13 @@ public class VelocityController : MonoBehaviour
 {
     Rigidbody2D rb;
     public string groundType;
+    public float dashSpeed;
+    public float dashTime;
+    float startDash_Time;
+    Vector2 oldspeed;
+    int dir;
+    bool ifDash;
+    int dashCount=0;
     
     void Start()
     {
@@ -15,10 +22,13 @@ public class VelocityController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        
         if (FindObjectOfType<CheckGround>().IsGrounded() == true)
         {
             if (Input.GetKey(KeyCode.D))
             {
+                dir = 1; // right dir
                 if (groundType == "N_Ground")
                 {
                     rb.velocity = rb.velocity + new Vector2(0.15f, -0.5f);
@@ -30,6 +40,7 @@ public class VelocityController : MonoBehaviour
             }
             if (Input.GetKey(KeyCode.A))
             {
+                dir = 2; // left dir
                 rb.velocity = rb.velocity + new Vector2(-0.15f, -0.5f);
             }
 
@@ -39,6 +50,47 @@ public class VelocityController : MonoBehaviour
             }
 
         }
-       
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCount==0)
+        {
+            dashCount = 1;
+            startDash_Time=0;
+            ifDash = true;
+            oldspeed.x = rb.velocity.x;
+
+            if (dir == 1)
+            {
+                rb.velocity = rb.velocity + new Vector2(30f, 0);   /*Vector2.right * dashSpeed*/
+            }
+            if (dir == 2)
+            {
+                rb.velocity = rb.velocity + new Vector2(-30f, 0);   /* + Vector2.left * dashSpeed;*/
+            }
+
+            /*startDash_Time = startDash_Time + Time.deltaTime;
+            if (startDash_Time > dashTime)
+            {
+                rb.velocity = oldspeed;
+            }*/
+            
+        }
+        if (dashCount == 1)
+        {
+            startDash_Time = startDash_Time + Time.deltaTime;
+            if (ifDash == true)
+            {
+                if (startDash_Time > dashTime)
+                {
+                    rb.velocity = new Vector2(oldspeed.x * 2, 0.1f);
+                    ifDash = false;
+                }
+            }
+            if (startDash_Time > 2f)
+            {
+                dashCount = 0;
+            }
+        }
+
     }
 }
