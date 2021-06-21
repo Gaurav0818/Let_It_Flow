3c61e65d5f4a0025f47444880160c47deecb5d03
Gaurav0818 <gaurav.ps.0818@gmail.com>
2021-06-21 13:54:08 +0530

    complete

---
+++ Assets/Script/GameOverMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void OnMainMenuButtonPress()
+    {
+        Debug.Log("menu button pressed");
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Script/Lvlskip_cheat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lvlskip_cheat : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Scene sceneLoaded = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(sceneLoaded.buildIndex);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Scene sceneLoaded = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(sceneLoaded.buildIndex + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
